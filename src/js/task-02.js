b1bf4d469c1d6557b21ce48abee56306181e5a5e
Vasyl Smutko <Curare16071969@gmail.com>
2022-10-28 01:37:18 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -9,9 +9,9 @@ const ingredients = [
 const innerThing = [];
 const list = document.querySelector("#ingredients");
 ingredients.forEach(ingredient => {
-const element = document.createElement("li");
-element.textContent = ingredient;
-element.class = "item";
-innerThing.push(element);
+  const element = document.createElement("li");
+  element.textContent = ingredient;
+  element.class = "item";
+  innerThing.push(element);
 })
 list.append(...innerThing);
