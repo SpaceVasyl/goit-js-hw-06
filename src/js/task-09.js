b4fa746600f578a4451222c09d49f8b2e5b0067e
Vasyl Smutko <Curare16071969@gmail.com>
2022-10-30 20:58:13 +0300

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,8 +1,10 @@
 const changeColorButton = document.querySelector(".change-color");
 const elementBody = document.querySelector("body")
 changeColorButton.addEventListener('click', getRandomHexColor);
-
+const textContentForSpan = document.querySelector(".color")
 
 function getRandomHexColor(event) {
-  elementBody.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  const blabla = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  elementBody.style.backgroundColor = blabla;
+  textContentForSpan.textContent = blabla;
 }
