986e09630e1fdda7697c4527e4e531df53d4a216
Vasyl Smutko <Curare16071969@gmail.com>
2022-10-30 19:12:51 +0300

    8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,16 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    if (event.currentTarget.email.value === "" || event.currentTarget.password.value === "") {
+        alert("Please fill in all the fields!")
+    }
+    const logInForm = {
+        Email: event.currentTarget.email.value,
+        Password: event.currentTarget.password.value,
+    }
+    console.log(logInForm);
+    event.currentTarget.reset();
+}
