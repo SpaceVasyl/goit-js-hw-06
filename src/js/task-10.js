40cf699424dd9f20527ee869ee27246b38f5f10f
Vasyl Smutko <Curare16071969@gmail.com>
2022-10-31 00:28:59 +0300

    10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,30 @@
+const demiurgButton = document.querySelector("[data-create]");
+const inputNumberOFBlocks = document.querySelector("input");
+const wrapperForNewEls = document.querySelector("#boxes");
+const destroyer = document.querySelector("[data-destroy]");
+demiurgButton.addEventListener('click', createBlocks);
+const boxes = [];
+function createBlocks(event) {
+  let size = 30;
+ for (let i = 0; i < inputNumberOFBlocks.value; i++) {
+   size = 30 + 10 * i;
+   const markUp = `<div class="newBox" style="width: ${size}px; height: ${size}px; background-color: ${getRandomHexColor()};"></div>`;
+   boxes.push(markUp);
+  }
+  wrapperForNewEls.insertAdjacentHTML("beforeend", boxes.join(""));
+}
+
+const wrapperForNewElsChildrens = document.querySelector("wrapperForNewEls.children")
+destroyer.addEventListener('click', destroyBoxes);
+function destroyBoxes(event) {
+  wrapperForNewEls.remove(wrapperForNewEls.children);
+}
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+// box.style.backgroundColor = getRandomHexColor();
+//    box.style.width = 30 + i * 10 + "px";
+//    box.style.height = 30 + i * 10 + "px";
