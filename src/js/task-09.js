0bb098fa961e14921250f5561bcec0b4ee48448b
Vasyl Smutko <Curare16071969@gmail.com>
2022-10-30 20:26:34 +0300

    9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,8 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const changeColorButton = document.querySelector(".change-color");
+const elementBody = document.querySelector("body")
+changeColorButton.addEventListener('click', getRandomHexColor);
+
+
+function getRandomHexColor(event) {
+  elementBody.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
